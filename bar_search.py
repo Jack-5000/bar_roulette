c6468b38fa0af061bb13f2e17974fd7e45969829
Jack-5000 <jackwylie713@gmail.com>
2025-09-02 21:21:32 -0400

    Google Places library
    Added in the libraries needed for the Google Places API

--- bar_search.py
+++ bar_search.py
@@ -1,7 +1,19 @@
+import os 
+from dotenv import load_dotenv
+from google.maps import places_v1
+
+
+load_dotenv()
+places_key = os.getenv("GOOGLE_PLACES_API")
+
+def geocoder(address: str):
+    print("Get long and lat")
+
+def bar_search(longitude: int, latitude: int, distance: int):
+
+    search_client = places_v1.PlacesAsyncClient(client_options={"api_key": str(places_key)})
 
 
 
 if __name__ == '__main__':
-    city = input("Enter your city: ")
-    state = input("Enter your State: ")
-    print("State = " + state + " City = " + city)
+    print("hello")
